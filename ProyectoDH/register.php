a214f49b5a5fbdc5239090c5adb9391bcf334eec
Catalina <catalina.picco@gmail.com>
2019-05-23 21:33:43 -0300

    Subi archivos del proyecto

---
+++ ProyectoDH/register.php
@@ -0,0 +1,14 @@
+<?php
+$titulo = "Registro";
+?>
+<!doctype html>
+<html lang="es">
+  <?php require_once ("head.php"); ?>
+
+  <body>
+    <h1>Hello, world!</h1>
+
+
+  <?php require_once ("scripts.php"); ?>
+  </body>
+</html>
