9f673d509f5947c36dc351e5f6bf5bd2b0dadf16
Maria Fernanda Aurelio <fer.aurelio@hotmail.com>
2019-05-25 22:31:07 -0300

    Subo el archivo header.php y las secciones con el codigo header embebido

--- ProyectoDH/header.php
+++ ProyectoDH/header.php
@@ -1,14 +1,9 @@
-<ul class="nav nav-pills">
-  <li class="nav-item">
-    <a class="nav-link active" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
-  </li>
-</ul>
+<?php
+$navLinks = [
+  "Perfil"=> "perfil.php"];
+$navAccess=[
+  "Faq" => "faq.php",
+  "Login" => "login.php",
+  "Registrarse" => "register.php",
+];
+ ?>
--- ProyectoDH/login.php
+++ ProyectoDH/login.php
@@ -1,12 +1,27 @@
 <?php
 $titulo = "Login";
+require_once ("header.php");
 ?>
+
 <!doctype html>
 <html lang="es">
   <?php require_once ("head.php"); ?>
 
   <body>
-    <h1>Hello, world!</h1>
+    <header> <!-- Encabezadp -->
+      <nav class="main-nav">
+        <ul class="nav-profile">
+          <?php foreach ($navLinks as $linkTxt => $linkUrl) : ?>
+            <li><i class="material-icons">account_circle</i><a href="<?php echo $linkUrl ?>">   <?php echo $linkTxt; ?> </a></li>
+          <?php endforeach; ?>
+        </ul>
+        <ul class="nav-access">
+          <?php foreach ($navAccess as $linkAcc => $links) : ?>
+            <li><a href="<?php echo $links; ?>"> <?php echo $linkAcc; ?> </a></li>
+          <?php endforeach; ?>
+        </ul>
+      </nav>
+    </header>
 
 
   <?php require_once ("scripts.php"); ?>
