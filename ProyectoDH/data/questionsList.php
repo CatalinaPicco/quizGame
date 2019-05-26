adea09c351fe723a68d93cc6a81b109a99559985
CatalinaPicco <catalina.picco@gmail.com>
2019-05-26 13:44:17 -0300

    agregue array de preguntas

---
+++ ProyectoDH/data/questionsList.php
@@ -0,0 +1,45 @@
+<?php
+
+$questionArrayList =
+[
+  [
+    'id' => '1',
+    'categoria' => 'general',
+    'pregunta' => '¿Cuál de los siguientes no es un sabor primario?',
+    'respuestas' => [
+      'Picante' => true,
+      'Dulce' => false,
+      'Amargo' => false,
+      'Salado' => false,
+    ],
+    'puntaje' => '1',
+  ],
+  [
+    'id' => '2',
+    'categoria' => 'literatura',
+    'pregunta' => ' ¿Quién escribió La Odisea?',
+    'respuestas' => [
+      'Homero' => true,
+      'Sócrates' => false,
+      'Platón' => false,
+      'Dante Alighieri' => false,
+    ],
+    'puntaje' => '1',
+  ],
+  [
+    'id' => '3',
+    'categoria' => 'general',
+    'pregunta' => '¿Cuál es el río más largo del mundo?',
+    'respuestas' => [
+      'Amazonas' => true,
+      'Nilo' => false,
+      'Sena' => false,
+      'Río de la Plata' => false,
+    ],
+    'puntaje' => '1',
+  ]
+];
+
+//var_dump($questionArrayList);
+
+?>
