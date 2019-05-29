98751fe298cf2e81cd90dc1fa59a58540290270a
Maria Fernanda Aurelio <fer.aurelio@hotmail.com>
2019-05-29 19:19:02 -0300

    agregando menu desplegable en header version mobile

--- ProyectoDH/header.php
+++ ProyectoDH/header.php
@@ -9,9 +9,21 @@ $navAccess=[
  ?>
     <header> <!-- Encabezadp -->
       <nav class="main-nav">
-        <a href="#" class="toggle-nav">
-          <span class="toggle-nav"><i class="material-icons">menu</i></span>
-        </a>
+<!-- probando el menu desplegable de bootstrap-->
+      <div class="toggle-nav">
+        <div class="btn-group">
+          <button type="button" class="btn  " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span ><i class="material-icons">menu</i></span>
+          </button>
+          <div class="dropdown-menu dropdown-menu-right">
+            <button class="dropdown-item" type="button">
+                <?php foreach ($navAccess as $linkAcc => $links) : ?>
+                  <li><a href="<?php echo $links; ?>"> <?php echo $linkAcc; ?> </a></li>
+                <?php endforeach; ?></button>
+          </div>
+        </div>
+</div>
+
         <span class="nav-profile-ico"><i class="material-icons">account_circle</i><a href="<?php echo $linkUrl ?>"></span>
         <ul class="nav-profile">
           <?php foreach ($navLinks as $linkTxt => $linkUrl) : ?>
