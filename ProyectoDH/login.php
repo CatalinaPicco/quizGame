e788cc38baf415b42d749981879fb08d0ac38824
alejandrodaquila@hotmail.com <alejandrodaquila@hotmail.com>
2019-05-29 21:13:34 -0300

    armado de base login.php y su css

--- ProyectoDH/login.php
+++ ProyectoDH/login.php
@@ -1,5 +1,5 @@
 <?php
-$titulo = "Registro";
+$titulo = "Login";
 ?>
 
 <!doctype html>
@@ -7,18 +7,25 @@ $titulo = "Registro";
   <?php require_once ("head.php"); ?>
 
   <body>
-<div class="container">
+  <div class="container">
   <!-- header -->
   <?php require_once ("header.php");?>
 
-
-
-<section>
-  <div class="button-play">
-    <a href="#">Jugar</a>
-  </div>
-</section>
-</div>
+    <div class="containerLogin">
+      <form class="" action="" method="post">
+        <input class="inputLogin" type="text" name="nameOrEmail" value="" placeholder="Usuario/Email">
+        <br>
+        <br>
+        <input class="inputLogin" type="password" name="pass" value="" placeholder="Password">
+        <br>
+        <br>
+        <input type="checkbox" id="rememberme" name="rememberme" value=""><label for="rememberme">Recordarme</label>
+        <br>
+        <br>
+        <input id="submitLogin" type="submit" name="" value="Iniciar sesión">  ¿Aún no tenés cuenta? <a href="register.php">Registrate</a>
+      </form>
+    </div>
+  </body>
 
   <?php require_once ("scripts.php"); ?>
   </body>
