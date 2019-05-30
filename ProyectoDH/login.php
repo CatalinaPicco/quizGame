a45811a312514027648f14825ba2a87c083cef8e
AleioD <alejandrodaquila@hotmail.com>
2019-05-30 11:50:23 -0300

    arreglo de login

--- ProyectoDH/login.php
+++ ProyectoDH/login.php
@@ -11,20 +11,6 @@ $titulo = "Login";
   <!-- header -->
   <?php require_once ("header.php");?>
 
-  <section class = "section-login">
-<h1 class = "h1-login">
-  Nombre del Juego
-</h1>
-<h2 class = "h2-login">
-  Aca va un subtitulo
-</h2>
-<img src="img/home/background-mobile.png" alt="Imagen del juego">
-  <div class="button-play">
-    <a href="#">Jugar</a>
-  </div>
-</section>
-</div>
-  <div class="container">
   <!-- header -->
   <?php require_once ("header.php");?>
 
