725a8478b743254cf2410bb2dca49b778dfcca99
Catalina <catalina.picco@gmail.com>
2019-06-03 20:34:42 -0300

    commit preguntas

---
+++ ProyectoDH/data/preguntas.php
@@ -0,0 +1,16 @@
+<?php
+$preguntas = array (
+  'id' => '1',
+  'categoria' => 'ciencia',
+  'pregunta' => '¿prueba?',
+  'respuestas' =>
+  array (
+    'respuesta_1' => 'true',
+    'respuesta_2' => 'false',
+    'respuesta_3' => 'false',
+    'respuesta_4' => 'false',
+  ),
+  'puntaje' => '1',
+)
+
+?>
