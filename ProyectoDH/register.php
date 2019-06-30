6cda6b6513a6444ebf9c4dd52fc7c88597af9615
Catalina <catalina.picco@gmail.com>
2019-06-30 19:40:43 -0300

    mejore estilos de formulario de registro

--- ProyectoDH/register.php
+++ ProyectoDH/register.php
@@ -33,107 +33,94 @@ if ($_POST) {
   <!-- header -->
   <?php require_once ("header.php");?>
 
-  <div class="container text-center mt-5 pt-5">
-
-    <div class="espacios-register">
-
-    </div>
-    <div>
-      <form class="ingreso" action="" method="post">
-        <p>
-          <label for="userName">
+  <div class="container mt-5 mb-5 pt-5">
+    <div class="row">
+      <form class="ingreso col-md-6 offset-md-3" action="" method="post">
+        <div class="form-group fontLatoLabel">
+          <label for="userName">Nombre de Usuario:
           </label>
-          <input class = "inputLogin" id="userName" type="text" name="userName"
+          <input class="form-control" id="userName" type="text" name="userName"
           value="<?= isset($userNameInPost) ? $userNameInPost : ''; ?>"
           placeholder="Nombre de usuario">
-        </p>
+        </div>
         <?php if (isset($erroresTotales["inUserName"])) : ?>
           <div class="alert alert-danger">
             <?= $erroresTotales["inUserName"]; ?>
           </div>
         <?php endif; ?>
-        <p>
-          <p>
-            <label for="nombre">
-            </label>
-            <input class="inputLogin" id="nombre" type="text" name="nombre"
-            value="<?= isset($fullNameInPost) ? $fullNameInPost : ''; ?>"
-            placeholder="Nombre">
-          </p>
-          <?php if (isset($erroresTotales["inFullName"])) : ?>
-            <div class="alert alert-danger">
-              <?= $erroresTotales["inFullName"]; ?>
-            </div>
-          <?php endif; ?>
-          <p>
-            <label for="email">
-            </label>
-            <input class="inputLogin" id="email" type="email" name="email"
-            value="<?= isset($emailInPost) ? $emailInPost : ''; ?>"
-            placeholder="Correo electrónico">
-          </p>
-          <?php if ( isset($erroresTotales["inEmail"]) ) : ?>
-            <div class="alert alert-danger">
-              <?= $erroresTotales["inEmail"]; ?>
-            </div>
-          <?php endif; ?>
-          <p>
-            <select class="inputLogin" name="pais">
-              <option value="">Elegí un país</option>
-              <?php foreach ($arrayPaises as $unPais) : ?>
-                <option value="<?= $unPais["name"] ?>"<?= isset($paisInPost) && $paisInPost == $unPais["name"] ? 'selected' : '';  ?>>
-                  <?= $unPais['name']?></option>
-                <?php endforeach ; ?>
-              </select>
-              <?php if ( isset($erroresTotales["inCountry"]) ) : ?>
-                <div class="alert alert-danger">
-                  <?= $erroresTotales["inCountry"]; ?>
-                </div>
-              <?php endif; ?>
-            </p>
-            <p>
-              <div class="">
-                <label for="avatar"><b>Imagen de perfil:</b></label>
-                <div class="form-group">
-                  <div class="inputLogin custom-file">
-                    <input type="file" name="avatar" class="custom-file-input">
-                    <label class="custom-file-label">elige una foto...</label>
-                  </div>
-                </div>
-                <?php if ( isset($erroresTotales["inAvatar"]) ) : ?>
-                  <div class="alert alert-danger">
-                    <?= $erroresTotales["inAvatar"]; ?>
-                  </div>
-                <?php endif; ?>
-              </div>
-            </p>
-            <p>
-              <label for="password">
-              </label>
-              <input class="inputLogin" id="password" type="password" name="password" value="" placeholder="Contraseña">
-            </p>
-            <?php if ( isset($erroresTotales["inPassword"]) ) : ?>
-              <div class="alert alert-danger">
-                <?= $erroresTotales["inPassword"]; ?>
-              </div>
-            <?php endif; ?>
-            <p>
-              <label for="repassword">
-              </label>
-              <input class="inputLogin" id="repassword" type="password" name="repassword" value="" placeholder="Repetir contraseña">
-            </p>
-            <?php if ( isset($errorsInRegister["inRepassword"]) ) : ?>
-              <div class="alert alert-danger">
-                <?= $errorsInRegister["inRepassword"]; ?>
-              </div>
-            <?php endif; ?>
-            <p>
-              <button class="btn btn-success" type="submit" name="button">Registrarse</button>
-            </p>
-          </form>
+        <div class="form-group fontLatoLabel">
+          <label for="nombre">Nombre:</label>
+          <input class="form-control" id="nombre" type="text" name="nombre"
+          value="<?= isset($fullNameInPost) ? $fullNameInPost : ''; ?>"
+          placeholder="Nombre">
         </div>
-
-
+        <?php if (isset($erroresTotales["inFullName"])) : ?>
+          <div class="alert alert-danger">
+            <?= $erroresTotales["inFullName"]; ?>
+          </div>
+        <?php endif; ?>
+        <div class="form-group fontLatoLabel">
+          <label for="email">Email:
+          </label>
+          <input class="form-control" id="email" type="email" name="email"
+          value="<?= isset($emailInPost) ? $emailInPost : ''; ?>"
+          placeholder="Correo electrónico">
+        </div>
+        <?php if ( isset($erroresTotales["inEmail"]) ) : ?>
+          <div class="alert alert-danger">
+            <?= $erroresTotales["inEmail"]; ?>
+          </div>
+        <?php endif; ?>
+        <div class="form-group fontLatoLabel">
+          <label for="pais">Pais:</label>
+          <select class=" form-control" name="pais">
+            <option value="">Elegí un país</option>
+            <?php foreach ($arrayPaises as $unPais) : ?>
+              <option value="<?= $unPais["name"] ?>"<?= isset($paisInPost) && $paisInPost == $unPais["name"] ? 'selected' : '';  ?>>
+                <?= $unPais['name']?></option>
+              <?php endforeach ; ?>
+            </select>
+            <?php if ( isset($erroresTotales["inCountry"]) ) : ?>
+              <div class="alert alert-danger">
+                <?= $erroresTotales["inCountry"]; ?>
+              </div>
+            <?php endif; ?>
+          </div>
+          <div class="form-group fontLatoLabel">
+            <label for="avatar">Sube un avatar: </label>
+            <input type="file" name="avatar" class="form-control-file">
+          </div>
+          <div>
+            <?php if ( isset($erroresTotales["inAvatar"]) ) : ?>
+              <div class="alert alert-danger">
+                <?= $erroresTotales["inAvatar"]; ?>
+              </div>
+            <?php endif; ?>
+          </div>
+          <div class="form-group fontLatoLabel">
+            <label for="password">Contraseña:
+            </label>
+            <input class="form-control" id="password" type="password" name="password" value="" placeholder="Contraseña">
+          </div>
+          <?php if ( isset($erroresTotales["inPassword"]) ) : ?>
+            <div class="alert alert-danger">
+              <?= $erroresTotales["inPassword"]; ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-group fontLatoLabel">
+            <label for="repassword">Repite contraseña:
+            </label>
+            <input class="form-control" id="repassword" type="password" name="repassword" value="" placeholder="Repetir contraseña">
+          </div>
+          <?php if ( isset($errorsInRegister["inRepassword"]) ) : ?>
+            <div class="alert alert-danger">
+              <?= $errorsInRegister["inRepassword"]; ?>
+            </div>
+          <?php endif; ?>
+          <button class="btn btn-success" type="submit" name="button">Registrarse</button>
+        </form>
 
       </div>
-      <?php require_once ("footer.php"); ?>
+
+    </div>
+    <?php require_once ("footer.php"); ?>
